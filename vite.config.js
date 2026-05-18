c117715cc025c48d9c898f158f76229070fde46d
Matheus Hirata <matheus.marostiga@gmail.com>
2026-05-18 00:03:06 -0300

    v0

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '192.168.31.217',
     proxy: {
       '/api': 'http://localhost:4000',
     },
